ef38b59dc5c52a0e35e99a1d6a02b63bcdcde448
darq37 <dzmudziak@gmail.com>
2018-11-17 18:19:08 +0100

    Druga wersja - duzo zmian

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdio>
 #include <ctime>
+#include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
@@ -9,30 +11,62 @@ int main() {
     int k; // krawedzie <Edges>
     char tryb;
     double czas; // zmienna do liczenia czasu
-    int tablica[k][n];
+
+
 
     cout << "wybierz tryb działania programu:" << '\n';
     cout << "Tryb A - wybierz a" << endl;
     cout << "Tryb B - wybierz b" << endl;
     cin >> tryb;
-    switch (tryb){
-        case 'a':
-            cout << "Wybrano tryb A!"<< endl;
-            int n;
+    switch (tryb) {
+        case 'a': {
+
+            cout << "Wybrano tryb A!" << endl;
             cout << "Podaj liczbą wierzchołków: " << endl;
             cin >> n;
-            int k;
-            k = static_cast<int>(0.7 * n);
-            cout <<"Wylosowano ilosc krawedzi: " << k << endl;
-            cout << "Zawartosc tablicy: " << endl;
+            bool tablica[n][n];
+            int edges[n];
+            for (int i = 0; i < n; ++i) {
+                edges[i] = i;
+            }
+
+            double kmin = 0.25 * n * (n - 1);
+            double kmax = 0.5 * n * (n - 1);
+            cout << "minimalna liczba krawędzi(by stworzyc spojny,pełny graf) dla danej liczny wierzcholkow to: "
+                 << ceil(kmin) << endl;
+            cout << "Maksymalna liczba krawędzi to: " << kmax << endl;
+            cout << "podaj liczbe krawędzi" << endl;
+            cin >> k;
+            while (k > kmax || k < kmin) {
+                cout << "Podałeś liczbę wierzchołków spoza zakresu!!!" << endl;
+                cout << "Wprowadź nową wartosć: " << endl;
+                cin >> k;
+            }
+            cout << "Wypelniam tablice... " << endl;
             for (int i = 0; i < n; i++) {
-                cout << "\n";
-                for (int j = 0; j < k; j++) {
-                    cout << 0 + (rand() % 2);
+                for (int j = 0; j < n; j++) {
+                    tablica[i][j] = false;
                 }
             }
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    tablica[i][i-1] = true;
+                    tablica[j][j+1] = true;
+                }
+            }
+
+
+            cout << "Zawartosc tablicy po wypelnieniu zerami: " << endl;
+            for (int i = 0; i < n; i++) {
+                cout << "\n";
+                for (int j = 0; j < n; j++) {
+                    cout << tablica[i][j];
+                }
+            }
+        }
             break;
-        case 'b':
+
+        case 'b': {
             cout << "Wybrano tryb B" << endl;
             cout << "Podaj liczbą wierzchołków: " << endl;
             cin >> n;
@@ -40,14 +74,18 @@ int main() {
             cout << "Podaj zapelnenie grafu: (0.5 - 1.0)" << endl;
             cin >> zapelnienie;
             k = static_cast<int>(zapelnienie * n);
-            cout << "Zapełnienie grafu wynosi " << zapelnienie*100 <<"%." << endl;
+            cout << "Zapełnienie grafu wynosi " << zapelnienie * 100 << "%." << endl;
             cout << "Graf ma teraz " << k << " krawędzi" << endl;
             break;
+        }
 
-        default:
+
+        default: {
             cout << "Nie wybrano odpowiedniego trybu!" << endl;
             break;
+        }
     }
+
     /*clock_t start;
     start = clock();
 
@@ -64,35 +102,5 @@ int main() {
     cout << '\n' << "Czas wykoania: " << czas << '\n';
      */
 
-    /*int score;
-
-    cout << "Score:";
-    cin >> score;
-
-    switch(score){
-        case 0:
-            cout << "a";
-            break;
-        case 1 ... 9:
-            cout << "b";
-            break;
-        case 11 ... 24:
-            cout << "c";
-            break;
-        case 25 ... 49:
-            cout << "d";
-            break;
-        case 50 ... 100:
-            cout << "e";
-            break;
-        default:
-            cout << "BAD VALUE";
-            break;
-    }
-    cout << endl;*/
-
-
-
     return 0;
 }
-
