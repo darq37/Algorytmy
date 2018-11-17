8fdc13cac1f39afe913a638ff295f81b152a9b5f
darq37 <dzmudziak@gmail.com>
2018-11-17 19:08:06 +0100

    Wypelnianie macierzy sasiedztwa w sposob losowy, az do uzyskania spojnosci grafu

--- main.cpp
+++ main.cpp
@@ -1,71 +1,80 @@
+#include <random>
+
 #include <iostream>
 #include <cstdio>
 #include <ctime>
 #include <cmath>
 #include <cstdlib>
+#include <random>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    int n; // wierzcholki <Vertex>
+    int n = 7; // wierzcholki <Vertex>
     int k; // krawedzie <Edges>
-    char tryb;
+    char tryb = 'a';
     double czas; // zmienna do liczenia czasu
 
-
+    int edges[n];
+    for (int i = 0; i < n; ++i) {
+        edges[i] = i;
+    }
 
     cout << "wybierz tryb działania programu:" << '\n';
     cout << "Tryb A - wybierz a" << endl;
     cout << "Tryb B - wybierz b" << endl;
-    cin >> tryb;
+    //cin >> tryb;
     switch (tryb) {
         case 'a': {
-
             cout << "Wybrano tryb A!" << endl;
             cout << "Podaj liczbą wierzchołków: " << endl;
-            cin >> n;
-            bool tablica[n][n];
-            int edges[n];
-            for (int i = 0; i < n; ++i) {
-                edges[i] = i;
-            }
-
-            double kmin = 0.25 * n * (n - 1);
-            double kmax = 0.5 * n * (n - 1);
-            cout << "minimalna liczba krawędzi(by stworzyc spojny,pełny graf) dla danej liczny wierzcholkow to: "
-                 << ceil(kmin) << endl;
-            cout << "Maksymalna liczba krawędzi to: " << kmax << endl;
+            //cin >> n;
+            int maksymalnaIloscKrawedzi = n * (n - 1) / 2;
+            int minimalnaIloscKrawedzi = static_cast<int>(ceil(0.25 * n * (n - 1)));
+            cout << "minimalna liczba krawędzi to: " << minimalnaIloscKrawedzi << endl;
+            cout << "Maksymalna liczba krawędzi to: " << maksymalnaIloscKrawedzi << endl;
             cout << "podaj liczbe krawędzi" << endl;
             cin >> k;
-            while (k > kmax || k < kmin) {
+            while (k > maksymalnaIloscKrawedzi || k < minimalnaIloscKrawedzi) {
                 cout << "Podałeś liczbę wierzchołków spoza zakresu!!!" << endl;
                 cout << "Wprowadź nową wartosć: " << endl;
                 cin >> k;
             }
-            cout << "Wypelniam tablice... " << endl;
+            cout << "Wypelniam macierz sąsiedztwa zerami... " << endl;
+            int macierzSasiedztwa[n][n];
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    tablica[i][j] = false;
-                }
-            }
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    tablica[i][i-1] = true;
-                    tablica[j][j+1] = true;
+                    macierzSasiedztwa[i][j] = 0;
                 }
             }
 
+            cout << "Tworze tablice permutacji krawędzi..." << endl;
+            int possibleEdges[n];
+            for (int i = 0; i < n; i++) {
+                possibleEdges[i] = i;
+            }
+            cout <<endl;
+            shuffle(possibleEdges, possibleEdges+n, mt19937(random_device()()));
+            cout << "Losowa permutacja: " << endl;
+            for (int i = 0; i < n; i++) {
+                cout << possibleEdges[i];
+            }
+            cout << endl << "Wypelniam tablice wedlug losowej permutacji... 0..n-1" << endl;
+            for (int i = 0; i < n - 1; ++i) {
+                macierzSasiedztwa[possibleEdges[i]][possibleEdges[i+1]] = 1; // wstawiamy liczbe pomiedzy dwoma wezlami grafu
+                macierzSasiedztwa[possibleEdges[i+1]][possibleEdges[i]] = 1; // wstawiamy liczbe symetrycznie po drugiej stronie
+            }
 
             cout << "Zawartosc tablicy po wypelnieniu zerami: " << endl;
             for (int i = 0; i < n; i++) {
                 cout << "\n";
                 for (int j = 0; j < n; j++) {
-                    cout << tablica[i][j];
+                    cout << macierzSasiedztwa[i][j];
                 }
             }
-        }
             break;
-
+        }
         case 'b': {
             cout << "Wybrano tryb B" << endl;
             cout << "Podaj liczbą wierzchołków: " << endl;
